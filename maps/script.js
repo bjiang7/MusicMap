48aead2748c79a09565dd6b0de4dc17c04a49f6a
lucyhyw <lucyhyw@gmail.com>
2016-11-12 22:26:09 -0500

    more styline

--- maps/script.js
+++ maps/script.js
@@ -64,29 +64,29 @@ var album_covers =
       })
       var styledMapType = new google.maps.StyledMapType(styles, {name: 'Styled'});
       map.mapTypes.set('Styled', styledMapType);
-      // Try HTML5 geolocation.
-      if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function(position) {
-          var pos = {
-            lat: position.coords.latitude,
-            lng: position.coords.longitude
-          };
+      // // Try HTML5 geolocation.
+      // if (navigator.geolocation) {
+      //   navigator.geolocation.getCurrentPosition(function(position) {
+      //     var pos = {
+      //       lat: position.coords.latitude,
+      //       lng: position.coords.longitude
+      //     };
 
-          map.setCenter(pos);
-        }, function() {
-          handleLocationError(true, infoWindow, map.getCenter());
-        });
-      } else {
-        // Browser doesn't support Geolocation
-        handleLocationError(false, infoWindow, map.getCenter());
-      }
+      //     map.setCenter(pos);
+      //   }, function() {
+      //     handleLocationError(true, infoWindow, map.getCenter());
+      //   });
+      // } else {
+      //   // Browser doesn't support Geolocation
+      //   handleLocationError(false, infoWindow, map.getCenter());
+      // }
 
-      function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-        infoWindow.setPosition(pos);
-        infoWindow.setContent(browserHasGeolocation ?
-          'Error: The Geolocation service failed.' :
-          'Error: Your browser doesn\'t support geolocation.');
-      }
+      // function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+      //   infoWindow.setPosition(pos);
+      //   infoWindow.setContent(browserHasGeolocation ?
+      //     'Error: The Geolocation service failed.' :
+      //     'Error: Your browser doesn\'t support geolocation.');
+      // }
     }
 
     initMap()
@@ -149,9 +149,6 @@ var album_covers =
 
       
     })
-
-
-
 
     function displayRoute(origin, destination, service, display) {
       var stops = [];
